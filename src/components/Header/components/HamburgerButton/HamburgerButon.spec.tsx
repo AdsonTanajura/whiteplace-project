88332b19207e82203f5afb433cf91633006c2dd8
AdsonTanajura <adson.tanajura@gmail.com>
2025-03-07 16:18:32 -0300

    Test: add test for LoginButton

--- src/components/Header/components/HamburgerButton/HamburgerButon.spec.tsx
+++ src/components/Header/components/HamburgerButton/HamburgerButon.spec.tsx
@@ -4,7 +4,7 @@ import HamburgerButton from '.';
 import { render } from '@testing-library/react';
 
 describe('<HamburguerButton>', () => {
-  it('should render HamburguerButtom', () => {
+  it('should render HamburguerButton', () => {
     const fn = jest.fn();
     const { queryAllByRole, container } = render(
       <HamburgerButton isOpen={false} setIsOpen={fn} />
@@ -22,11 +22,11 @@ describe('<HamburguerButton>', () => {
       <HamburgerButton isOpen={true} setIsOpen={fn} />
     );
 
-    const hamburgerButtomClose = getByTestId('hamburger-button-close');
+    const hamburgerButtonClose = getByTestId('hamburger-button-close');
     const hamburgerButtonsOpen = queryAllByTestId('hamburger-button-open');
 
     expect(container).toMatchSnapshot();
-    expect(hamburgerButtomClose).toBeVisible();
+    expect(hamburgerButtonClose).toBeVisible();
     expect(hamburgerButtonsOpen).toHaveLength(0);
   });
 
@@ -37,34 +37,11 @@ describe('<HamburguerButton>', () => {
       <HamburgerButton isOpen={false} setIsOpen={fn} />
     );
 
-    const hamburgerButtomOpen = getByTestId('hamburger-button-open');
-    const hamburgerButtomClose = queryAllByTestId('hamburger-button-close');
+    const hamburgerButtonOpen = getByTestId('hamburger-button-open');
+    const hamburgerButtonClose = queryAllByTestId('hamburger-button-close');
 
     expect(container).toMatchSnapshot();
-    expect(hamburgerButtomOpen).toBeVisible();
-    expect(hamburgerButtomClose).toHaveLength(0);
+    expect(hamburgerButtonOpen).toBeVisible();
+    expect(hamburgerButtonClose).toHaveLength(0);
   });
 });
-
-// it('should show the HamburgerButton open', () => {
-//   const fn = jest.fn();
-
-//   const { container, queryAllByRole } = render(
-//     <HamburgerButton isOpen={true} setIsOpen={fn} />
-//   );
-
-//   expect(container).toMatchSnapshot();
-//   const buttons = queryAllByRole('button', { name: /Login/i });
-//   expect(buttons).toHaveLength(1);
-// });
-
-// it('buttom teste', () => {
-//   const fn = jest.fn();
-//   const { container, queryAllByRole } = render(
-//     <HamburgerButton isOpen={false} setIsOpen={fn} />
-//   );
-
-//   expect(container).toMatchSnapshot();
-//   const buttons = queryAllByRole('button', { name: /Login/i });
-//   expect(buttons).toHaveLength(0);
-// });
